97884d1c5806b597fee011b91134a44bc7785268
Govindhkiruthi <63140385+Govindhkiruthi@users.noreply.github.com>
2023-01-19 13:14:17 +0530

    Add files via upload

--- src/test/java/com/testvagrant/Base/BaseClass.java
+++ src/test/java/com/testvagrant/Base/BaseClass.java
@@ -1,25 +1,35 @@
 package com.testvagrant.Base;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
 import org.json.simple.JSONArray;
 import org.testng.annotations.BeforeSuite;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testvagrant.utils.CommonUtils;
+import com.testvagrant.utils.TeamsModel;
+import com.testvagrant.utils.DeserializeJsonUtility;
 
 
 public class BaseClass {
 
-	CommonUtils commonUtils = new CommonUtils();
-	
+	protected CommonUtils commonUtils = new CommonUtils();
+	protected DeserializeJsonUtility jsonutility = new DeserializeJsonUtility();
 	String absolutePath = Paths.get("src","test","resources").toFile().getAbsoluteFile().toString();
 	String filepath = absolutePath+ "\\input.json";
-	protected JSONArray playersList;
+	public TeamsModel teamsModel;
+	
+	
 	
 	@BeforeSuite
 	public void beforeSuite()
 	{
-		 playersList = commonUtils.getJSONArrayFromJsonFile(filepath);
+		teamsModel = jsonutility.deserializeJson(filepath);
+		 
 	}
 }
