b35c3f4efd982aff7552b8babbf7fb69bdd340d3
Govindhkiruthi <63140385+Govindhkiruthi@users.noreply.github.com>
2023-01-19 13:17:13 +0530

    Add files via upload

--- src/test/java/com/testvagrant/utils/CommonUtils.java
+++ src/test/java/com/testvagrant/utils/CommonUtils.java
@@ -1,23 +1,6 @@
 package com.testvagrant.utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CommonUtils {
 	
